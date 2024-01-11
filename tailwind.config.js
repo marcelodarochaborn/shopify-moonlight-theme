307fc7dc19bc0a518696cb2c8a089aa202433aca
marcelo.darocha <marcelo.darocha@borngroup.com>
2024-01-11 11:19:40 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './**/*.{js,json,liquid}'
+  ],
+  safelist: [
+    
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
